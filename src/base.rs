a5b9460b70ca9fc99df25b3c966ecf74f1e90ffb
Andrew <afreema4@live.unc.edu>
2023-03-09 16:47:12 -0500

    Better error handling for interfacing with other programs

--- src/base.rs
+++ src/base.rs
@@ -3,6 +3,7 @@ use std::io::{Read};
 use std::net::{TcpStream, ToSocketAddrs};
 use std::os::unix::net::UnixStream;
 use num_derive::FromPrimitive;
+use thiserror::Error;
 
 #[allow(dead_code, unused_imports)]
 #[path = "./ioheader_generated.rs"]
@@ -26,67 +27,31 @@ mod triggers_generated;
 
 const MAGIC_NUMBER: &str = "#!AER-DAT4.0\r\n";
 
-#[derive(Debug)]
-pub struct ParseError {
-    message: String,
+
+#[allow(missing_docs)]
+#[derive(Error, Debug)]
+pub enum ParseError {
+    #[error("Parse error: `{0}`")]
+    General(String),
+
+    #[error("Unsupported stream type: `{0}`")]
+    UnsupportedStreamType(String),
+
+    #[error("FlatBuffer error")]
+    FlatBuffer(#[from] flatbuffers::InvalidFlatbuffer),
+
+    #[error("Utf8 error")]
+    Utf8(#[from] std::str::Utf8Error),
+
+    #[error("RoxmlTree error")]
+    RoxmlTree(#[from] roxmltree::Error),
+
+    #[error("ParseIntError error")]
+    ParseInt(#[from] std::num::ParseIntError),
+
+    #[error("IO error")]
+    Io(#[from] std::io::Error),
 }
-
-impl ParseError {
-    pub fn new(message: &str) -> ParseError {
-        ParseError {
-            message: message.to_string(),
-        }
-    }
-}
-
-impl std::fmt::Display for ParseError {
-    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(formatter, "{}", self.message)
-    }
-}
-
-impl std::convert::From<std::io::Error> for ParseError {
-    fn from(error: std::io::Error) -> Self {
-        ParseError {
-            message: error.to_string(),
-        }
-    }
-}
-
-impl std::convert::From<flatbuffers::InvalidFlatbuffer> for ParseError {
-    fn from(error: flatbuffers::InvalidFlatbuffer) -> Self {
-        ParseError {
-            message: error.to_string(),
-        }
-    }
-}
-
-
-impl std::convert::From<std::str::Utf8Error> for ParseError {
-    fn from(error: std::str::Utf8Error) -> Self {
-        ParseError {
-            message: error.to_string(),
-        }
-    }
-}
-
-impl std::convert::From<roxmltree::Error> for ParseError {
-    fn from(error: roxmltree::Error) -> Self {
-        ParseError {
-            message: error.to_string(),
-        }
-    }
-}
-
-impl std::convert::From<std::num::ParseIntError> for ParseError {
-    fn from(error: std::num::ParseIntError) -> Self {
-        ParseError {
-            message: error.to_string(),
-        }
-    }
-}
-
-
 
 trait Source:std::io::Read {}
 impl Source for File {}
@@ -108,7 +73,7 @@ impl StreamContent {
             "FRME" => Ok(StreamContent::Frame),
             "IMUS" => Ok(StreamContent::Imus),
             "TRIG" => Ok(StreamContent::Triggers),
-            _ => Err(ParseError::new("unsupported stream type")),
+            _ => Err(ParseError::UnsupportedStreamType("unsupported stream type".to_string())),
         }
     }
 }
@@ -157,8 +122,8 @@ impl Decoder {
             let mut magic_number_buffer = [0; MAGIC_NUMBER.len()];
             decoder.file.read_exact(&mut magic_number_buffer)?;
             if std::str::from_utf8(&magic_number_buffer)? != MAGIC_NUMBER {
-                return Err(ParseError::new(
-                    "the file does not contain AEDAT4 data (wrong magic number)",
+                return Err(ParseError::General(
+                    "the file does not contain AEDAT4 data (wrong magic number)".to_string(),
                 ));
             }
             decoder.position += MAGIC_NUMBER.len() as i64;
@@ -212,15 +177,15 @@ fn read_io_header(mut decoder: Decoder) -> Result<Decoder, ParseError> {
         decoder.file_data_position = ioheader.file_data_position();
         let description = match ioheader.description() {
             Some(content) => content,
-            None => return Err(ParseError::new("the description is empty")),
+            None => return Err(ParseError::General("the description is empty".to_string())),
         };
         let document = roxmltree::Document::parse(description)?;
         let dv_node = match document.root().first_child() {
             Some(content) => content,
-            None => return Err(ParseError::new("the description has no dv node")),
+            None => return Err(ParseError::General("the description has no dv node".to_string())),
         };
         if !dv_node.has_tag_name("dv") {
-            return Err(ParseError::new("unexpected dv node tag"));
+            return Err(ParseError::General("unexpected dv node tag".to_string()));
         }
         let output_node = match dv_node.children().find(|node| {
             node.is_element()
@@ -228,16 +193,16 @@ fn read_io_header(mut decoder: Decoder) -> Result<Decoder, ParseError> {
                 && node.attribute("name") == Some("outInfo")
         }) {
             Some(content) => content,
-            None => return Err(ParseError::new("the description has no output node")),
+            None => return Err(ParseError::General("the description has no output node".to_string())),
         };
         for stream_node in output_node.children() {
             if stream_node.is_element() && stream_node.has_tag_name("node") {
                 if !stream_node.has_tag_name("node") {
-                    return Err(ParseError::new("unexpected stream node tag"));
+                    return Err(ParseError::General("unexpected stream node tag".to_string()));
                 }
                 let stream_id = match stream_node.attribute("name") {
                     Some(content) => content,
-                    None => return Err(ParseError::new("missing stream node id")),
+                    None => return Err(ParseError::General("missing stream node id".to_string())),
                 }
                     .parse::<u32>()?;
                 let identifier = match stream_node.children().find(|node| {
@@ -248,10 +213,10 @@ fn read_io_header(mut decoder: Decoder) -> Result<Decoder, ParseError> {
                     Some(content) => match content.text() {
                         Some(content) => content,
                         None => {
-                            return Err(ParseError::new("empty stream node type identifier"))
+                            return Err(ParseError::General("empty stream node type identifier".to_string()))
                         }
                     },
-                    None => return Err(ParseError::new("missing stream node type identifier")),
+                    None => return Err(ParseError::General("missing stream node type identifier".to_string())),
                 }
                     .to_string();
                 let mut width = 0u16;
@@ -263,7 +228,7 @@ fn read_io_header(mut decoder: Decoder) -> Result<Decoder, ParseError> {
                             && node.attribute("name") == Some("info")
                     }) {
                         Some(content) => content,
-                        None => return Err(ParseError::new("missing info node")),
+                        None => return Err(ParseError::General("missing info node".to_string())),
                     };
                     width = match info_node.children().find(|node| {
                         node.is_element()
@@ -272,9 +237,9 @@ fn read_io_header(mut decoder: Decoder) -> Result<Decoder, ParseError> {
                     }) {
                         Some(content) => match content.text() {
                             Some(content) => content,
-                            None => return Err(ParseError::new("empty sizeX attribute")),
+                            None => return Err(ParseError::General("empty sizeX attribute".to_string())),
                         },
-                        None => return Err(ParseError::new("missing sizeX attribute")),
+                        None => return Err(ParseError::General("missing sizeX attribute".to_string())),
                     }
                         .parse::<u16>()?;
                     height = match info_node.children().find(|node| {
@@ -284,9 +249,9 @@ fn read_io_header(mut decoder: Decoder) -> Result<Decoder, ParseError> {
                     }) {
                         Some(content) => match content.text() {
                             Some(content) => content,
-                            None => return Err(ParseError::new("empty sizeX attribute")),
+                            None => return Err(ParseError::General("empty sizeX attribute".to_string())),
                         },
-                        None => return Err(ParseError::new("missing sizeX attribute")),
+                        None => return Err(ParseError::General("missing sizeX attribute".to_string())),
                     }
                         .parse::<u16>()?;
                 }
@@ -302,13 +267,13 @@ fn read_io_header(mut decoder: Decoder) -> Result<Decoder, ParseError> {
                     )
                     .is_some()
                 {
-                    return Err(ParseError::new("duplicated stream id"));
+                    return Err(ParseError::General("duplicated stream id".to_string()));
                 }
             }
         }
     }
     if decoder.id_to_stream.is_empty() {
-        return Err(ParseError::new("no stream found in the description"));
+        return Err(ParseError::General("no stream found in the description".to_string()));
     }
     Ok(decoder)
 
@@ -374,17 +339,17 @@ impl Iterator for Decoder {
                     Err(error) => return Some(Err(ParseError::from(error))),
                 }
             }
-            _ => return Some(Err(ParseError::new("unknown compression algorithm"))),
+            _ => return Some(Err(ParseError::General("unknown compression algorithm".to_string()))),
         }
         let expected_content = &(match self.id_to_stream.get(&packet.stream_id) {
             Some(content) => content,
-            None => return Some(Err(ParseError::new("unknown stream id"))),
+            None => return Some(Err(ParseError::General("unknown stream id".to_string()))),
         }
             .content);
         if !flatbuffers::buffer_has_identifier(&packet.buffer, &expected_content.to_string(), true)
         {
-            return Some(Err(ParseError::new(
-                "the stream id and the identifier do not match",
+            return Some(Err(ParseError::General(
+                "the stream id and the identifier do not match".to_string(),
             )));
         }
         Some(Ok(packet))
